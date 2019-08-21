6c27201613c0a1431a580627488a72085a25f2f1
NikitOs <exemplum777@gmail.com>
2019-08-22 00:36:08 +0300

    Plus: change name, auto-height and css code

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
-var allList = [
-  {id: 1, name: 'Learn HTML/CSS', about: 'I need choose a good book about Web-programming, HTML, CSS and JS', date: new Date()},
-  {id: 2, name: 'Learn JavaScript', about: 'I need choose a good book about Web-programming, HTML, CSS and JS', date: new Date()},
-  {id: 3, name: 'Learn Node.JS', about: 'I need choose a good book about Web-programming, HTML, CSS and JS', date: new Date()}
-]
-for (var i = 0; i < allList.length; i++) {
-  let key = allList[i].id + '; ' + allList[i].name;
-  // document.querySelector('#root').appendChild(key);switch
-  // console.log(key);
+setInterval(function() {
+  var autoHeight = window.innerHeight;
+  document.querySelector('.all-god').style.height = autoHeight + 'px';
+}, 1)
+
+
+function changeTextName() {
+  var userName = document.querySelector('.user-name');
+  var textName = document.querySelector('.user-name-change');
+  textName.style.width = (userName.clientWidth + 20) + 'px';
+  textName.style.display = 'block';
+  textName.value = userName.innerHTML;
+  userName.style.display = 'none';
+  window.onkeypress = function() {changeName(event, userName, textName);};
+}
+
+function changeName(event, userName, textName) {
+  event = event || window.event;
+  var elem = event.target;
+  if (event.keyCode == 13) {
+    textName.style.display = 'none';
+    userName.style.display = null;
+    userName.innerHTML = textName.value;
+  }
 }
