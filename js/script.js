1192dc68b86da64d9fc7bb603f2a90b8a12ee145
NikitOs <exemplum777@gmail.com>
2019-08-23 14:38:21 +0300

    Real functional for change name and status

--- js/script.js
+++ js/script.js
@@ -2,3 +2,46 @@ setInterval(function() {
   var autoHeight = window.innerHeight;
   document.querySelector('.all-god').style.height = autoHeight + 'px';
 }, 1)
+
+
+var userName = document.querySelector('.user-name');
+userName.addEventListener("click", function() {changeTextName(this)} );
+
+var userStatus = document.querySelector('.user-status');
+userStatus.addEventListener("click", function() {changeTextName(this)} );
+
+function changeTextName(elem) {
+  var elemCss = getComputedStyle(elem);
+
+  var input = document.querySelector('.change-input');
+  input.style.fontSize = elemCss.fontSize;
+  input.style.width = (elem.clientWidth + 20) + 'px';
+  input.style.display = 'block';
+  input.value = elem.innerHTML;
+  input.focus();
+
+  elem.style.display = 'none';
+
+  input.onkeydown = function() {setName(elem, input)};
+  input.onblur = function() {noneSetName(elem, input)};
+}
+
+
+function setName(elem, input) {
+
+  if (event.keyCode == 13) {
+    input.style.display = 'none';
+    elem.style.display = null;
+    elem.innerHTML = input.value;
+  } else if (event.keyCode == 27) {
+    input.style.display = 'none';
+    elem.style.display = null;
+    elem.innerHTML = elem.innerHTML;
+  }
+}
+
+function noneSetName(elem, input) {
+  input.style.display = 'none';
+  elem.style.display = null;
+  elem.innerHTML = elem.innerHTML;
+}
