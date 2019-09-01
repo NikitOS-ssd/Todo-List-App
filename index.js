8f05a53674bdf461b9f9cecfc477ccf7b5ec014c
NikitOs <exemplum777@gmail.com>
2019-09-01 18:44:02 +0300

    Make a login page

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+app.use(express.static(__dirname + '/public')); //Подключает модули с CSS и JS к обычным файлам
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-  res.render('index.ejs');
+  res.render('login.ejs');
 });
 
+app.get('/home', function(req, res) {
+  res.render('index.ejs');
+});
 
 app.listen(3000);
