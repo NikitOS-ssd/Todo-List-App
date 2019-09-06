ef8545f492037548774550c77a977ad0f50ba8ce
NikitOs <exemplum777@gmail.com>
2019-09-06 11:40:22 +0300

    Some change in project

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-
+var userObj = 'Unknown';
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(express.static(__dirname + '/public')); //Подключает модули с CSS и JS к обычным файлам
@@ -13,18 +13,19 @@ app.get('/', function(req, res) {
 });
 
 app.get('/home', function(req, res) {
-  res.render('index.ejs');
+  res.render('index.ejs', {userName: 'Unknown'});
 });
 
 app.post('/login', urlencodedParser, function(req, res) {
   if(req.body.password == '') {
     res.render('login.ejs');
+    console.log('Вход не выполнен');
   } else {
     console.log(req.body);
     let signDate = new Date();
     console.log(`${req.body.login} вошёл в систему в ${signDate.getHours()}:${signDate.getMinutes()}`);
 
-    res.render('index.ejs', {userObj: req.body});
+    res.render('index.ejs', {userName: req.body.login});
   }
 });
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,3 @@
-// setInterval(function() {
-//   var autoHeight = window.innerHeight;
-//   document.querySelector('.all-god').style.height = autoHeight + 'px';
-// }, 1);
-
-
 var userName = document.querySelector('.user-name');
 userName.addEventListener("click", function() {changeTextName(this)} );
 
@@ -48,25 +42,10 @@ function noneSetName(elem, input) {
   elem.innerHTML = elem.innerHTML;
 }
 
-
+//ПОДСЧЁТ ЗАДАЧ
 numberOfLists();
 function numberOfLists() {
   var allLists = document.querySelectorAll('.block-list');
   var listsNumber = document.querySelector('.numb-of-lists');
   listsNumber.innerHTML = allLists.length;
 }
-
-// autoHeightBlockList();
-// function autoHeightBlockList() {
-//   var allList = document.querySelectorAll('.block-list');
-//
-//   for (var i = 0; i < allList.length; i++) {
-//     var list = allList[i];
-//     let img = list.querySelector('.img-list');
-//     if (img) {
-//       list.style.height = '350px';
-//     } else {
-//       continue;
-//     }
-//   }
-// }
