cc19780edd248b70f2999aac2680e16bf988ae32
NikitOs <exemplum777@gmail.com>
2019-09-17 10:37:00 +0300

    Something changes

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ var app = express();
 var userObj = 'Unknown';
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-app.use(express.static(__dirname + '/public')); //Подключает модули с CSS и JS к обычным файлам
+
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public')); //Подключает модули с CSS и JS к обычным файлам
 
 app.get('/', function(req, res) {
   res.render('login.ejs');
@@ -29,4 +30,42 @@ app.post('/login', urlencodedParser, function(req, res) {
   }
 });
 
-app.listen(3000);
+var arrUser = {
+  'naruto777': {
+    name: 'Nikitos',
+    age: 19,
+    city: 'Moscow',
+    profession: 'programmist'
+  },
+  'tanya-tulpina': {
+    name: 'Tanya',
+    age: 19,
+    city: 'Stavropol',
+    profession: 'jurnalist'
+  }
+};
+
+
+const usersRouter = express.Router();
+
+usersRouter.use('/:userid/about', function(req, res) {
+  var userId = req.params.userid;
+  var info = '<ul>';
+  if (arrUser[userId]) {
+    for (var key in arrUser[userId]) {
+      info += `<li>${arrUser[userId][key]}</li>`;
+    }
+    info += '</ul>';
+    res.send(info);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+usersRouter.use('/:userid/globaly', function(req, res) {
+  res.send(`Id - ${req.params.userid}`);
+});
+
+app.use('/user', usersRouter);
+
+app.listen(3030);
